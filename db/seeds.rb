a8ad8a7e83277247fabc8632f5e8c12513be8614
Andy Maskiell <maskiella@gmail.com>
2018-02-23 20:54:14 +0000

    Created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,37 @@
 user1 = User.create(username: "saurookadook", email: "maskiella@gmail.com", password: "ilovecatz")
+
+platforms_list = {
+  "Steam" = {},
+  "PS4" = {},
+  "XboxOne" = {},
+  "Nintendo 3DS" = {},
+  "Nintendo Switch" = {},
+  "PS3" = {},
+  "Xbox360" = {},
+  "Nintendo DS" = {},
+  "Nintendo WiiU" = {},
+  "Mobile" = {}
+}
+
+platforms_list.each do |name, platform_hash|
+  Platform.create(name: name)
+  # p = Platform.new
+  # p.name = name
+  # p.save
+end
+
+genres_list = {
+  "Action" = {},
+  "Adventure" = {},
+  "MMO" = {},
+  "RPG" = {},
+  "Simulation" = {},
+  "Strategy" = {}
+}
+
+genres_list.each do |name, genre_hash|
+  Genre.create(name: name)
+  # p = Genre.new
+  # p.name = name
+  # p.save
+end
