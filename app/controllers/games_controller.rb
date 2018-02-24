13dbe7fdf2dd25991547a58e4027e6282ad94507
Andy Maskiell <maskiella@gmail.com>
2018-02-24 00:56:33 +0000

    Started building signup, login, and show pages

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,14 @@ class GamesController < ApplicationController
 
   use Rack::Flash
 
+  get '/games' do
+    @current_user = current_user
+    @users = User.all
+    @games = Game.all
+    erb :"games/list_games"
+  end
+
   get '/games/:slug' do
-    
+    @current_user = current_user
   end
 end
