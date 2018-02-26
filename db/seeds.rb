e21add210ae45cc88cd89ce234d85387b8797cd4
Andy Maskiell <maskiella@gmail.com>
2018-02-26 20:48:15 +0000

    Corrected issues in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,31 +39,28 @@ genres_list.each do |name, genre_hash|
 end
 
 owned_game1 = OwnedGame.create(title: "Doom", genre_id: 4, platform_id: 2, notes: "Found lots of big guns")
-owned_game2 = OwnedGame.create(title: "Doom", genre_id: 4, platform_id: 3)
-archived_game1 = ArchivedGame.create(title: "Doom", genre_id: 4, platform_ids: [1, 2, 3])
-
 owned_game3 = OwnedGame.create(title: "Mystic Messenger", genre_id: 5, platform_id: 10)
-archived_game2 = ArchivedGame.create(title: "Mystic Messenger", genre_id: 5, platform_ids: [10])
-
+owned_game3 = OwnedGame.create(title: "Mystic Messenger", genre_id: 5, platform_id: 10, notes: "Literally my favorite game")
 owned_game4 = OwnedGame.create(title: "Hello Kitty: Big City Dreams", genre_id: 5, platform_id: 8, notes: "ZOMG FOUND THE CUTEST OUTFIT =^.^=")
+owned_game5 = OwnedGame.create(title: "Witcher 3: Wild Hunt", genre_id: 4, platform_id: 1, notes: "Have best Gwent deck ever. Literally no one can beat me >:]")
+owned_game6 = OwnedGame.create(title: "Witcher 3: Wild Hunt", genre_id: 4, platform_id: 2, notes: "Geralt's so dreamy.... <3<br><br>And Yennefer is the best")
+owned_game7 = OwnedGame.create(title: "Hearthstone", genre_id: 6, platform_id: 10, notes: "Cubelock op....")
+owned_game8 = OwnedGame.create(title: "Hearthstone", genre_id: 6, platform_id: 1, notes: "All of my decks are terrible. Halp!")
+
+archived_game1 = ArchivedGame.create(title: "Doom", genre_id: 4, platform_ids: [1, 2, 3])
+archived_game2 = ArchivedGame.create(title: "Mystic Messenger", genre_id: 5, platform_ids: [10])
 archived_game3 = ArchivedGame.create(title: "Hello Kitty: Big City Dreams", genre_id: 5, platform_ids: [8])
-
-owned_game5 = OwnedGame.create(title: "Witcher 3: Wild Hunt", genre_id: 4, platform_id: 2, notes: "Have best Gwent deck ever. Literally no one can beat me >:]")
 archived_game4 = ArchivedGame.create(title: "Witcher 3: Wild Hunt", genre_id: 4, platform_ids: [1, 2, 3])
-
-owned_game6 = OwnedGame.create(title: "Hearthstone", genre_id: 6, platform_id: 10, notes: "Cubelock op....")
-owned_game7 = OwnedGame.create(title: "Hearthstone", genre_id: 6, platform_id: 1, notes: "All of my decks are terrible. Halp!")
 archived_game5 = ArchivedGame.create(title: "Hearthstone", genre_id: 6, platform_ids: [1, 10])
 
 
 user1.owned_games << owned_game1
 user1.owned_games << owned_game5
-user1.owned_games << owned_game6
+user1.owned_games << owned_game7
 
-user2.owned_games << owned_game3
+user2.owned_games << owned_game2
 user2.owned_games << owned_game4
 
-user3.owned_games << owned_game1
 user3.owned_games << owned_game3
-user3.owned_games << owned_game5
-user3.owned_games << owned_game7
+user3.owned_games << owned_game6
+user3.owned_games << owned_game8
