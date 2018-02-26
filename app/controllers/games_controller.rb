bc3bd9ef2fa45e8044bb076fd0af4f5f3efcf1b1
Andy Maskiell <maskiella@gmail.com>
2018-02-26 22:34:25 +0000

    Added more to post/games route

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -36,36 +36,46 @@ class GamesController < ApplicationController
   # end
 
   post '/games' do
-    @user = User.find_by_id(session[:id])
+    puts params
+    @user = User.find(session[:user_id])
     if title == ""
       flash[:message] = "You must enter a title for the game you'd like to add to your Journal. Please try again."
-      redirect to '/games/add_game'
+      redirect to '/games/new'
     elsif !params[:game][:platform_id].nil? || params[:platform] != ""
       flash[:message] = "You must either select a platform or create a new one, and can only associate one platform with the created game."
-      redirect to '/games/add_game'
+      redirect to '/games/new'
     elsif !params[:game][:genre_id].nil? || params[:genre] != ""
       flash[:message] = "You must either select a genre or create a new one, and can only associate one genre with the created game."
-      redirect to '/games/add_game'
+      redirect to '/games/new'
     end
 
     @owned_game = OwnedGame.create(title: params[:title])
     @archived_game = ArchivedGame.find_or_create_by(title: params[:title])
 
     if params[:game][:platform_id] != nil
-      @platform = Platform.find_by_id(params[:game][:platform_id])
+      @platform = Platform.find(params[:game][:platform_id])
     elsif params[:platform] != ""
       @platform = Platform.find_or_create_by(name: params[:platform])
     elsif params[:game][:genre_id] != nil
-      @genre = Genre.find_by_id(params[:game][:genre_id])
+      @genre = Genre.find(params[:game][:genre_id])
     elsif params[:genre] != ""
       @genre = Genre.find_or_create_by(name: params[:genre])
     end
 
-      @user.owned_games << @owned_game
-      # @user.owned_games << @owned_game if !@user.owned_games.include?(@owned_game)
-    end
+    @owned_game.platform = @platform
+    @owned_game.genre = @genre
+    @owned_game.notes = params[:notes]
+    @owned_game.save
 
-    redirect to "/users/show"
+    @archived_game.platforms << @platform if !(@archived_game.platforms.include?(@platform))
+    @archived_game.genre = @genre if !(@archived_game.genre == @genre)
+    @archived_game.save
+
+
+    @user.owned_games << @owned_game
+    # @user.owned_games << @owned_game if !@user.owned_games.include?(@owned_game)
+
+    redirect to "/users/#{@user.slug}"
   end
 
   patch '/games/:slug' do
